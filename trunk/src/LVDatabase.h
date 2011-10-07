e6551db3ee91ad02065bc5d10942a8c7a75fff4f
homola.p@gmail.com <homola.p@gmail.com@2df471a3-61d5-8437-082e-1e298cb242c3>
2011-10-07 10:30:46 +0000

    initial commit

---
+++ trunk/src/LVDatabase.h
@@ -0,0 +1,18 @@
+//
+//  LVDatabase.h
+//  LevelDBWrapper
+//
+//  Created by Petr Homola on 10/7/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LVDatabase : NSObject
+
+- (id)initWithPath:(NSString*)path;
+- (BOOL)putValue:(NSString*)value forKey:(NSString*)key;
+- (NSString*)valueForKey:(NSString*)key;
+- (BOOL)deleteValueForKey:(NSString*)key;
+
+@end
